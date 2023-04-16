bfaef4db7a05e53abe516602a7a6414af7d03d27
ValentynKrasovskyi <krasovskyi00@gmail.com>
2023-04-16 16:26:54 +0200

    part 1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
+const ingredientsList = document.querySelector('#ingredients');
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientElements = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+ingredientsList.append(...ingredientElements);
